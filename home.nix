e69fb56139a18e1389c2d1ec6960819dd9a2fa96
rivera-bl <rivera.pablo1090@gmail.com>
2022-07-24 13:44:20 +0000

    alacritty: basic configuration

--- home.nix
+++ home.nix
@@ -24,10 +24,12 @@
     BROWSER = "firefox";
     TERM = "alacritty";
   };
-
+    
   xsession.windowManager.i3 = {
+    config.terminal = "alacritty";
     enable = true;
     package = pkgs.i3-gaps;
+    config.bars = [];
     extraConfig = builtins.readFile ./i3/config;
   };
 
@@ -144,6 +146,66 @@
 
   programs.alacritty = {
     enable = true;
-    settings = {};
+    settings = {
+
+      window = {
+        padding = {
+          x = 8.0;
+          y = 6.0;
+        };
+        opacity = 1.0;
+      };
+
+      scrolling = {
+        history = 10000;
+        multiplier = 3;
+      };
+      
+      selection = {
+        save_to_clipboard = false;
+      };
+
+      font = {
+        size = 8.0;
+        draw_bold_text_with_bright_colors = false;
+        normal = {
+          family = "monospace";
+          style = "Regular";
+        };
+        bold = {
+          family = "monospace";
+          style = "Bold";
+        };
+        italic = {
+          family = "monospace";
+          style = "Italic";
+        };
+        bold_italic = {
+          family = "monospace";
+          style = "Bold Italic";
+        };
+        glyph_offset = {
+          x = 0.0;
+          y = 0.0;
+        };
+
+        colors = {
+          primary = {};
+          cursor = {};
+          vi_mode_cursor = {};
+          line_indicator = {};
+          footer_bar = {};
+          selection = {};
+          search = {};
+          hints = {};
+          normal = {};
+          bright = {};
+          dim = {};
+        };
+
+        bell = {};
+
+      };
+    };
   };
 }
